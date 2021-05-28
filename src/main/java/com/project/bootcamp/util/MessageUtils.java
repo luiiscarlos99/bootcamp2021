435cae18625c99446fd2775622784fda75ed8d7f
Luis <luis@MacBook-Air.local>
2021-05-28 17:32:59 -0300

    Criacao de repositorio com o codigo do bootcamp

---
+++ src/main/java/com/project/bootcamp/util/MessageUtils.java
@@ -0,0 +1,10 @@
+package com.project.bootcamp.util;
+
+
+public abstract class MessageUtils {
+
+    public static final String STOCK_ALREADY_EXISTS = "Stock already exists in the database";
+
+    public static final String NOT_RECORDS_FOUND = "No records found";
+
+}
